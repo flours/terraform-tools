7717eaa5268eb5d228aabcea255861edcbd47b14
flours <n-tada@fusic.co.jp>
2024-10-07 15:19:12 +0900

    feat: コメントアウト

--- server/terraform/env/main/main.tf
+++ server/terraform/env/main/main.tf
@@ -14,16 +14,16 @@ terraform {
 }
 
 # EC2 インスタンスを作成
-resource "aws_instance" "example" {
-  ami           = "ami-0ef29ab52ff72213b"
-  instance_type = "t2.micro"               # インスタンスタイプ
+# resource "aws_instance" "example" {
+#   ami           = "ami-0ef29ab52ff72213b"
+#   instance_type = "t2.micro"               # インスタンスタイプ
 
-  tags = {
-    Name = "example-instance"              # インスタンスに付けるタグ
-  }
-}
+#   tags = {
+#     Name = "example-instance"              # インスタンスに付けるタグ
+#   }
+# }
 
-# 出力変数（インスタンスの公開 IP を表示）
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
-}
+# # 出力変数（インスタンスの公開 IP を表示）
+# output "instance_public_ip" {
+#   value = aws_instance.example.public_ip
+# }
